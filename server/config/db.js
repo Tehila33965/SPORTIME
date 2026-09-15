f763d63661d7f6d29a3d24d2c3dd193e9f4b3b12
DESKTOP-F6TEN9G <efrat37764@gmail.com>
2026-09-15 23:41:53 +0300

    Initialize server, setup .env configuration, and connect to MongoDB Atlas

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+import { connect } from "mongoose";
+import { env } from "./env.js";
+
+export const connectDB = async () => {
+    try {
+        await connect(env.MONGO_URI);
+        console.log('mongo connected successfully');
+    } 
+    catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
---
+++ server/config/env.js
@@ -0,0 +1,5 @@
+import 'dotenv/config';
+
+export const env = {
+    MONGO_URI: process.env.MONGO_URI,
+};
