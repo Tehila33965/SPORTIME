b6c7553986198db8936dc7994a0b15f00a5342ce
Tehila <tehila@gmail.com>
2026-09-16 00:12:19 +0300

    Solving the DB connection issue

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,8 @@
 import { connect } from "mongoose";
 import { env } from "./env.js";
+import dns from "dns";  // זה מה שגרם לשרת להתחבר למסד הנתונים
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 export const connectDB = async () => {
     try {
